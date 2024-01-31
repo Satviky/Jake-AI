71cde57fbafe1f608fffcbf225548636033f4f54
Satviky <126392608+Satviky@users.noreply.github.com>
2024-01-31 22:51:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,9 +3,7 @@ import os
 import discord
 import openai
 
-
-with open('chat.txt', 'r') as f:
-  chat = f.read() 
+chat= "Jake has been on the run from the government for four years. He is wanted for revealing things that some people prefer to keep hidden. On the 4th year of his run Hannah video calls him and that time someone kidnaps Hannah. Jake first contacts user after giving them access to a private chat between Jessy and Dan in the Prologue of the game, He believes that user is the key to finding her, he video calls the user and gives them an explanation of the situation. He asks user to decrypt Hannah's phone cloud andshare anything of significance with him. user and Jake find out various clues leading towards Hannah's friends. Sometimes, either of them would start small talk during their time spent investigating, with Jake admitting he has not had small talk in a long time. If user purchases the Multimedia Premium package, Jake and the user have the opportunity to become a couple, falling in love over the course of their investigation. Over the course of the investigation, user discover that Jake is half-brother to Lilly and Hannah, making his full name Jake Donfort. He is in or near Duskwood for the duration, at one point requiring assistance to protect himself."
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
 token = os.getenv("dc")
